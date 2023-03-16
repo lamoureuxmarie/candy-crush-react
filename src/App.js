82b9abe77879b25b566ab0b53e772e3251ef884e
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-03-16 21:50:55 +0100

    create function to generate random colour for candy board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 
+const width = 8
+const candyColours = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
+
 function App() {
+
+  const createBoard = () => {
+    for (let i = 0; i < width * width; i++) {
+      const randomColour = candyColours[Math.floor(Math.random() * candyColours.length)]
+      console.log(randomColour);
+    }
+  }
+
+  createBoard();
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
     </div>
   );
 }
